4d7c01d80d69ffd538dc766bbb155191e9b05e48
Malesio <ewenelain@hotmail.fr>
2021-05-25 17:04:47 +0200

    Add chatbot admin controller

---
+++ admin/controller.js
@@ -0,0 +1,10 @@
+class ChatbotController {
+
+    static async init() {
+        const instance = new ChatbotController();
+
+        return instance;
+    }
+};
+
+module.exports = ChatbotController;
