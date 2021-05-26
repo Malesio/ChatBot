ef2fbd2d352db9b6b24d5ac378b719cfea3edd8a
Malesio <ewenelain@hotmail.fr>
2021-05-26 18:54:17 +0200

    Chatbot controller implementation with LowDB

--- admin/controller.js
+++ admin/controller.js
@@ -1,7 +1,36 @@
+require("dotenv").config();
+
+const Low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
+const Bot = require("../models/bot");
+
 class ChatbotController {
 
+    constructor(database) {
+        this.db = database;
+    }
+
+    async createBot(botData) {
+        const newId = this.db.get("next_id").value();
+        botData.id = newId;
+
+        const newBot = new Bot(botData);
+
+        await this.db.get("bots").push(newBot).write();
+        await this.db.set("next_id", newId + 1).write();
+    }
+
+    async getBots() {
+
+    }
+
     static async init() {
-        const instance = new ChatbotController();
+        const adapter = new FileAsync(process.env["BOTS_DB_FILE"]);
+        const db = await Low(adapter);
+
+        await db.defaults({ bots: [], next_id: 0 }).write();
+
+        const instance = new ChatbotController(db);
 
         return instance;
     }
