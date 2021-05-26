8629f1ed43393a7b845e7357c2dcfcf86c5f416d
LacieBernkastel <xhimitsu.lacie@gmail.com>
2021-05-26 17:37:16 +0200

    moved view + user model added

---
+++ models/user.js
@@ -0,0 +1,15 @@
+class User{
+
+    constructor(data){
+        
+        if(undefined != data.login){
+            this.login = this.login;
+        }
+
+        this.data = new Map();
+        
+    }
+
+}
+
+module.exports = User;
