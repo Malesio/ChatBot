93b8142519053766590b889663d027ee79e06212
Malesio <ewenelain@hotmail.fr>
2021-06-01 15:48:40 +0200

    Receive POST requests as JSON

--- admin/server.js
+++ admin/server.js
@@ -4,6 +4,7 @@ const router = require("./router");
 const app = express();
 const port = process.env["CHATBOT_ADMIN_PORT"] || 7777;
 
+app.use(express.json());
 app.use("/chatbot", router);
 
 app.get("/", async (req, res) => {
