d427f1c9c52c362cc72b1e2ac01b77040c08048b
Malesio <ewenelain@hotmail.fr>
2021-06-07 00:58:01 +0200

    Create basic discord.js bot

---
+++ admin/discord/bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+require("dotenv").config();
+
+class Client {
+    constructor() {
+        this.client = new Discord.Client();
+        this.prefix = process.env["DISCORD_BOT_PREFIX"];
+        this.client.on("message", Client.prototype.gotMessage);
+    }
+
+    gotMessage(message) {
+        if (message.content.startsWith(this.prefix) && !message.author.bot) {
+            const userMessage = message.content.slice(this.prefix.length);
+            // TODO: submit the user's message to Rivescript
+        }
+    }
+
+    run() {
+        this.client.login(process.env["DISCORD_BOT_TOKEN"]);
+    }
+
+    stop() {
+        this.client.destroy();
+    }
+}
+
+module.exports = Client;
