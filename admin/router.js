4317d363b6fbdf84340f501191ac26acb55b6524
Malesio <ewenelain@hotmail.fr>
2021-06-07 00:56:14 +0200

    Add brain & interface management routes

--- admin/router.js
+++ admin/router.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const Controller = require("./controller");
 const router = express.Router();
@@ -39,6 +40,54 @@ Controller.init().then(controller => {
                 res.sendStatus(204);
             } else {
                 res.status(404).send({error: `Could not find chatbot with id ${req.params.id} to delete`});
+            }
+        } catch (e) {
+            res.status(400).send({error: e.message});
+        }
+    });
+
+    router.get("/brains", async (req, res) => {
+        const brains = fs.readdirSync(__dirname + "/../brains");
+    
+        res.status(200).send({brains: brains.map(b => b.slice(0, b.indexOf(".")))});
+    });
+
+    router.put("/:id/brain", async (req, res) => {
+        try {
+            const bot = await controller.getBot(req.params.id);
+            if (bot) {
+                await controller.addBrain(bot.id, req.body.brain);
+                res.sendStatus(204);
+            } else {
+                res.status(404).send({error: `Could not find chatbot with id ${req.params.id}`});
+            }
+        } catch (e) {
+            res.status(400).send({error: e.message});
+        }
+    });
+
+    router.put("/:id/interface/discord", async (req, res) => {
+        try {
+            const bot = await controller.getBot(req.params.id);
+            if (bot) {
+                await controller.addDiscordInterface(bot.id);
+                res.status(200).send({invite: "<discord invite link here>"});
+            } else {
+                res.status(404).send({error: `Could not find chatbot with id ${req.params.id}`});
+            }
+        } catch (e) {
+            res.status(400).send({error: e.message});
+        }
+    });
+
+    router.delete("/:id/interface/discord", async (req, res) => {
+        try {
+            const bot = await controller.getBot(req.params.id);
+            if (bot) {
+                await controller.deleteDiscordInterface(bot.id);
+                res.sendStatus(204);
+            } else {
+                res.status(404).send({error: `Could not find chatbot with id ${req.params.id}`});
             }
         } catch (e) {
             res.status(400).send({error: e.message});
