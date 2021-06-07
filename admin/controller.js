f63b944bd187cfe898d999847a85ab1f69bdd1bb
Malesio <ewenelain@hotmail.fr>
2021-06-07 15:08:47 +0200

    Refactoring & correcting Discord bot management

--- admin/controller.js
+++ admin/controller.js
@@ -11,6 +11,7 @@ class ChatbotController {
     constructor(database) {
         this.db = database;
         this.discordClient = new DiscordBot();
+        this.discordClient.run();
     }
 
     async createBot(botData) {
@@ -45,15 +46,8 @@ class ChatbotController {
     }
 
     async deleteBot(botId) {
-        botId = parseInt(botId, 10);
-        if (isNaN(botId)) {
-            throw new Error("Invalid bot ID");
-        }
-
-        const bots = this.db.get("bots");
-
-        if (bots.find({id: botId}).value()) {
-            await bots.remove({id: botId}).write();
+        if (await this.getBot(botId)) {
+            await this.db.get("bots").remove({id: botId}).write();
 
             return true;
         }
@@ -72,9 +66,9 @@ class ChatbotController {
             throw new Error(`Brain ${brain} not found in brain library`);
         }
 
-        const brains = this.db.get("bots").find({id: parseInt(botId, 10)}).get("brain");
+        const brains = (await this.getBot(botId)).brain;
 
-        if (brains.value().includes(brain)) {
+        if (brains.includes(brain)) {
             throw new Error(`Bot already has ${brain} brain`);
         }
 
@@ -82,25 +76,23 @@ class ChatbotController {
     }
 
     async addDiscordInterface(botId) {
-        const interfaces = this.db.get("bots").find({id: parseInt(botId, 10)}).get("interface");
+        const interfaces = (await this.getBot(botId)).interface;
 
-        if (interfaces.value().includes("discord")) {
+        if (interfaces.includes("discord")) {
             throw new Error("Bot already has a Discord interface");
         }
 
-        this.discordClient.run();
+        // TODO: allow bot to chat on discord
 
         await interfaces.push("discord").write();
     }
 
     async deleteDiscordInterface(botId) {
-        const interfaces = this.db.get("bots").find({id: parseInt(botId, 10)}).get("interface");
+        const interfaces = (await this.getBot(botId)).interface;
 
-        if (!interfaces.value().includes("discord")) {
+        if (!interfaces.includes("discord")) {
             throw new Error("Bot does not have a Discord interface");
         }
-
-        this.discordClient.stop();
 
         await interfaces.pull("discord").write();
     }
