e0e35ccef1dd5c3667bea555f051c63bf045bb9f
Malesio <ewenelain@hotmail.fr>
2021-06-07 17:59:24 +0200

    Delegate Rive management to data controller

---
+++ admin/dataController.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+
+const low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
+const {default: RiveScript} = require("rivescript");
+
+class DataController {
+    constructor(db) {
+        this.db = db;
+        this.riveInstances = new Map();
+    }
+
+    async reloadRiveInstance(bot) {
+        const rive = this.riveInstances.get(bot.id);
+        await rive.loadFile(bot.brain.map(f => __dirname + `/../brains/${f}.rive`));
+        rive.sortReplies();
+    }
+
+    async createRiveInstance(bot) {
+        const rive = new RiveScript();
+        this.riveInstances.set(bot.id, rive);
+
+        await this.reloadRiveInstance(bot);
+    }
+
+    async deleteRiveInstance(botId) {
+        this.riveInstances.delete(botId);
+    }
+
+    async getBotReply(botId, userName, message) {
+        await this.loadUserData(botId, userName);
+
+        const reply = await this.riveInstances.get(botId).reply(userName, message);
+
+        await this.saveUserData(botId, userName);
+
+        return reply;
+    }
+
+    async loadUserData(botId, userName) {
+        const userData = this.db.get(`userdata.${userName}`).value();
+
+        if (userData) {
+            this.riveInstances.get(botId).setUservars(userName, userData);
+        }
+    }
+
+    async saveUserData(botId, userName) {
+        const userVars = this.riveInstances.get(botId).getUservars(userName);
+
+        await this.db.get(`userdata.${userName}`).assign(userVars).write();
+    }
+
+    static async init() {
+        const adapter = new FileAsync(process.env["USERDATA_DB_FILE"]);
+        const db = await low(adapter);
+
+        await db.defaults({ userdata: {} }).write();
+
+        const instance = new DataController(db);
+
+        return instance;
+    }
+}
+
+module.exports = DataController;
