3c822f94dbbd1a2f64ec6a84b48e33ec252c5d8f
Malesio <ewenelain@hotmail.fr>
2021-06-07 17:59:45 +0200

    Use data controller to handle responses

--- admin/discord/bot.js
+++ admin/discord/bot.js
@@ -3,16 +3,21 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 class Client {
-    constructor() {
+    constructor(dataController) {
+        this.dataController = dataController;
+
         this.client = new Discord.Client();
         this.prefix = process.env["DISCORD_BOT_PREFIX"];
         this.client.on("message", Client.prototype.gotMessage);
+        this.activeBot = -1;
     }
 
     gotMessage(message) {
-        if (message.content.startsWith(this.prefix) && !message.author.bot) {
+        if (message.content.startsWith(this.prefix) && !message.author.bot && this.activeBot !== -1) {
             const userMessage = message.content.slice(this.prefix.length);
             // TODO: submit the user's message to Rivescript
+            this.dataController.getBotReply(this.activeBot, message.author.username, userMessage)
+                .then(message.channel.send);
         }
     }
 
